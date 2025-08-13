ec7b6353ab1ea96b4197f154df5c96b0ccf64540
ToxicalNoob3062 <rahat3062@gmail.com>
2025-08-13 12:06:28 -0400

    feat: Add Note component and integrate it into the App layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import type { Component } from "solid-js";
 import Header from "./components/header";
 import Features from "./components/features";
 import Registration from "./components/registration";
+import Note from "./components/note";
 
 const App: Component = () => {
   return (
-    <div class="max-w-screen-2xl mx-auto p-2 lg:p-4 font-lilita bg-[#faf7eb] flex flex-col gap-10">
+    <div class="max-w-screen-2xl mx-auto p-2 lg:p-4 font-lilita bg-[#faf7eb] flex flex-col gap-10 relative">
       <Header />
       <Features />
       <Registration />
+      <Note/>
     </div>
   );
 };
---
+++ src/components/note.tsx
@@ -0,0 +1,9 @@
+export default function Note() {
+  return (
+    <h3 class="absolute left-0 top-0 w-full z-50 p-2 xl:p-3 lg:-mt-4 xl:top-10
+               bg-blue-400 text-white text-center lg:text-xl xl:text-2xl">
+      Hurray! 🎉 We have extended our early bird offer till
+      <span class="text-amber-200 mx-2">31st August, 2025</span>. 🪄
+    </h3>
+  );
+}
